3763bba6ae765f7e0c3c04a38af44a179e656c98
zhou996996 <166315134+zhou996996@users.noreply.github.com>
2025-01-28 14:30:10 -0500

    Update pda-functions.R

--- pda-functions.R
+++ pda-functions.R
@@ -8,7 +8,7 @@ ipw_bootstrap_function <- function(formula_d_e, formula_n_e, formula_d_l, formul
                              data = .dat %>% filter(intervention_lag==0 & tstart <early_num), 
                              family = binomial(),
                              weights = Freq)
-  #am 6/23: try using dat. the reason this is also good idea in general is that your function shouldn't depend on anything in the "global" environment
+  
   .dat$p_d_early <- NA
   .dat[.dat$intervention_lag==0 & .dat$tstart < early_num,]$p_d_early <- predict(ipw_denominator_early, 
                                                                                  newdata = .dat %>% filter(intervention_lag==0 & tstart < early_num), 
@@ -17,7 +17,6 @@ ipw_bootstrap_function <- function(formula_d_e, formula_n_e, formula_d_l, formul
   .dat[.dat$intervention_lag==0 & .dat$tstart < early_num,]$p_n_early <- predict(ipw_numerator_early, 
                                                                                  newdata = .dat %>% filter(intervention_lag==0 & tstart < early_num), 
                                                                                  type = "response") 
-  #dat$p_n_early <- predict(ipw_numerator_early, newdata = dat, type = "response") 
   
   ipw_denominator_late <- glm(formula = as.formula(formula_d_l), 
                               data = .dat %>% filter(intervention_lag==0 & tstart >= early_num & tstart <=14), 
@@ -49,7 +48,7 @@ ipw_bootstrap_function <- function(formula_d_e, formula_n_e, formula_d_l, formul
   
   .dat <- .dat %>%
     mutate(sw_late=case_when(tstart < early_num & intervention_lag == 1 ~ 0,    ##8/22: that's where the weight code for later group got changed.
-                             tstart < early_num & ade_res_day==tstart & ade_res_day < early_num & intervention_lag != 1 & intervention==0 ~ 1, #added ade_res_day
+                             tstart < early_num & ade_res_day==tstart & ade_res_day < early_num & intervention_lag != 1 & intervention==0 ~ 1, 
                              tstart < early_num& intervention_lag != 1 & intervention_lag != 1 & intervention==0 ~ (1 - p_n_early) / (1 - p_d_early),
                              tstart < early_num & intervention_lag != 1 & intervention==1 ~ 0,
                              tstart >= early_num & tstart < 14 & ade_res_day < 14 & intervention_lag != 1 & intervention==0 ~ 1,
@@ -59,7 +58,7 @@ ipw_bootstrap_function <- function(formula_d_e, formula_n_e, formula_d_l, formul
                              tstart >= 14 ~ 1)) %>%
     group_by(study.id) %>% mutate(sw_late_prod=cumprod(sw_late)) %>% ungroup()
   
-  return(.dat)} # this asks the function to give you "dat" once it is done running
+  return(.dat)} 
 
 
 analysis_function <- function(.dat, 
@@ -77,8 +76,8 @@ analysis_function <- function(.dat,
     N <- length(unique.ids)
     set.seed(bs_num) # keeps replicable
     weighted.ids.table <- table(sample(unique.ids, size=N, replace=T)) %>% as.data.frame(., stringsAsFactors=FALSE) %>% 
-      mutate(study.id=as.numeric(Var1)) %>% dplyr::select(-Var1) ### note that sum(weighted.ids.table$Freq) == N as expected
-    .dat <- left_join(weighted.ids.table, .dat, by="study.id") ### now joins only what weighted.ids.table has (instead of left_join(date, weighted.ids.table,)
+      mutate(study.id=as.numeric(Var1)) %>% dplyr::select(-Var1) 
+    .dat <- left_join(weighted.ids.table, .dat, by="study.id") 
   } else if(!bootstrap | bs_num==0){ #point estimate only (no bootstrap)
     .dat <- .dat %>% mutate(Freq=1)
   }
@@ -98,7 +97,7 @@ analysis_function <- function(.dat,
   pooled_ipw_early <- glm(formula = as.formula(.formula_outcome), 
                           data=.dat, 
                           family=quasibinomial(),
-                          weights=sw_early_prod*Freq) #### look at this line/step and same in line 106 to understand why all bootstrap resample values were the same previously
+                          weights=sw_early_prod*Freq) 
   
   pooled_ipw_late <- glm(formula = as.formula(.formula_outcome),    
                          data=.dat, 
@@ -116,5 +115,5 @@ analysis_function <- function(.dat,
                         late= 1- cumprod(p_late)) %>%
     mutate(rd=early-late) %>% mutate(rr=early/late) %>%
     mutate(across(everything(), ~ round(., 4)))
-  return(results) ### am: 6/24, kept as data.frame instead of kable for post-processing in a different step
+  return(results) 
 }
