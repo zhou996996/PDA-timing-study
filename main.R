fef1eb2843a5c09e9041fbb433c32bae8387e1c7
zhou996996 <166315134+zhou996996@users.noreply.github.com>
2025-02-26 12:05:35 -0500

    Update main.R

--- main.R
+++ main.R
@@ -87,29 +87,3 @@ sensitivity_analysis3 <- overall_function_sensi3(.dat=pda_intubated_long_sensiti
                                                  formula_outcome=.formula_outcome_sensi3,
                                                  point_estimate_only=FALSE,
                                                  bs_reps=1000)
-
-#sensitivity analysis for surgical ligation vs. device closure
-.formula_d_e_sensi4 = "intervention==1 ~ ns(tstart, knots=c(3), Boundary.knots=c(1,4)) + 
-                                 ns(ega_ref, knots=c(27), Boundary.knots=c(25,29)) +
-                                 ns(ega_days, knots=171, Boundary.knots=c(165, 182)) + 
-                                 ns(ref_year, knots=2017, Boundary.knots=c(2016, 2021)) +
-                                 ns(weight, knots=620, Boundary.knots=c(530, 840)) +
-                                 female + ref_med"
-.formula_n_e_sensi4 = "intervention==1 ~ ns(tstart, knots=c(3), Boundary.knots=c(1,4))"
-.formula_d_l_sensi4 = "intervention==1 ~ ns(tstart, knots=c(3), Boundary.knots=c(1,9)) + 
-                                ns(ega_ref, knots=c(27), Boundary.knots=c(25,29)) +
-                                ns(ega_days, knots=171, Boundary.knots=c(165, 182)) +
-                                ns(ref_year, knots=2017, Boundary.knots=c(2016, 2021)) +
-                                ns(weight, knots=620, Boundary.knots=c(530, 840)) +
-                                female + ref_med"
-.formula_n_l_sensi4 = "intervention==1 ~ ns(tstart, knots=3, Boundary.knots = c(1,9))"
-.formula_outcome_sensi4 = "extubation==1 ~ ns(tstart, knots = c(5,10), Boundary.knots = c(2, 40))"
-
-sensitivity_analysis4 <- overall_function_sensi6(.dat=pda_intubated_long, 
-                                                 formula_d_e=.formula_d_e_sensi4,
-                                                 formula_n_e=.formula_n_e_sensi4,
-                                                 formula_d_l=.formula_d_l_sensi4,
-                                                 formula_n_l=.formula_n_l_sensi4,
-                                                 formula_outcome=.formula_outcome_sensi4,
-                                                 point_estimate_only=FALSE,
-                                                 bs_reps=1000)
